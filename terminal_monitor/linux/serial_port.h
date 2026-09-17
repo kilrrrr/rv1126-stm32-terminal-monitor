e5985993749b1e06ddb3001c04f076e48cf0c66a
kilrrrr <lzyxll1031@163.com>
2026-09-17 15:24:01 +0800

    add terminal_monitor/linux/serial_port.h

---
+++ terminal_monitor/linux/serial_port.h
@@ -0,0 +1,13 @@
+#ifndef TERMINAL_MONITOR_SERIAL_PORT_H
+#define TERMINAL_MONITOR_SERIAL_PORT_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int serial_open(const char *path, int baudrate);
+int serial_close(int fd);
+int serial_write_all(int fd, const uint8_t *data, size_t length);
+int serial_read_poll(int fd, uint8_t *data, size_t capacity, int timeout_ms);
+
+#endif
+
