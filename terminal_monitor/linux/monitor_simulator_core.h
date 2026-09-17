64ecd8b98d94e954747ae36932d63fc871c5cb8a
kilrrrr <lzyxll1031@163.com>
2026-09-17 15:55:56 +0800

    feat: add no-hardware monitor simulator

---
+++ terminal_monitor/linux/monitor_simulator_core.h
@@ -0,0 +1,24 @@
+#ifndef TERMINAL_MONITOR_SIMULATOR_CORE_H
+#define TERMINAL_MONITOR_SIMULATOR_CORE_H
+
+#include "../common/protocol.h"
+
+typedef struct
+{
+	uint16_t sequence;
+	uint32_t uptime_ms;
+	uint32_t interval_ms;
+	uint32_t frame_count;
+	uint32_t drop_every;
+	uint32_t bad_crc_every;
+	terminal_status_t status;
+} monitor_simulator_state_t;
+
+void monitor_simulator_init(monitor_simulator_state_t *state,
+	uint32_t interval_ms, uint32_t drop_every, uint32_t bad_crc_every);
+
+int monitor_simulator_next_frame(monitor_simulator_state_t *state,
+	uint8_t *output, size_t capacity, size_t *output_length);
+
+#endif
+
